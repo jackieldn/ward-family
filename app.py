98de1d049c719c8df6ab07a25341ecd95ddc59ad
Jack <jack.ward@tandpgroup.com>
2025-02-10 14:11:07 +0000

    app.py update for deployment

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from savings import savings_bp, init_savings_db
 app = Flask(__name__)
 app.register_blueprint(savings_bp)
 
+#Allow external access
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
+
 # Main Dashboard
 @app.route('/')
 def index():
@@ -456,6 +460,5 @@ def get_titles():
         return jsonify({"error": "Database error"}), 500
     
 
-if __name__ == '__main__':
-    init_savings_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
